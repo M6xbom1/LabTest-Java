fcfd6cc36062ea327240862bccebd68c0deef932
Jet <87537009+M6xbom1@users.noreply.github.com>
2022-09-25 13:23:48 +0700

    Add files via upload

---
+++ BalancingSymbolsTest.java
@@ -0,0 +1,69 @@
+import javax.swing.JOptionPane;
+
+public class BalancingSymbolsTest {
+    public static void main(String[] args) throws Exception {
+
+        //ArrayStack stack = new ArrayStack();
+        LinkedStack stack = new LinkedStack();
+
+        String ex = JOptionPane.showInputDialog(null, "What is your expression?");
+        try{
+            for (int i = 0; i < ex.length(); i++) {
+                char c = ex.charAt(i);
+                if(c == '(' || c == '{' || c == '['){
+                    stack.push(c);
+                    continue;
+                }
+                char check;
+                switch(c){
+                    case ')':
+                    if(stack.isEmpty()){
+                        throw new Exception("Stack is empty :Not Balancing Symbol");
+                    }
+                        check = (char)stack.top();
+                        if(check == '('){
+                            stack.pop();
+                        }
+                        else if(check == '{' || check == '['){
+                            throw new Exception("Symbol does not match at position "+(i+1)+": Not Balancing Symbol");
+                        }
+                        break;
+
+                    case '}':
+                    if(stack.isEmpty()){
+                        throw new Exception("Stack is empty :Not Balancing Symbol");
+                    }
+                        check = (char)stack.pop();
+                        if(check == '{'){
+                            stack.pop();
+                        }
+                        else if(check == '(' || check == '['){
+                            throw new Exception("Symbol does not match at position "+(i+1)+": Not Balancing Symbol");
+                        }
+                        break;
+
+                    case ']':
+                    if(stack.isEmpty()){
+                        throw new Exception("Stack is empty :Not Balancing Symbol");
+                    }
+                        check = (char)stack.pop();
+                        if(check == '['){
+                            stack.pop();
+                        }
+                        else if(check == '(' || check == '{'){
+                            throw new Exception("Symbol does not match at position "+(i+1)+": Not Balancing Symbol");
+                        }
+                        break;
+                    }
+                }
+            if(stack.isEmpty()){
+                throw new Exception("Balancing Symbol");
+            }
+            else{
+                throw new Exception("Illegal Expression : Not Balancing Symbol");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,e.getMessage());
+        }
+    }
+}
---
+++ MyStack.java
@@ -0,0 +1,15 @@
+interface MyStack {
+    public  void MyStack();
+
+    public void push(Object e) throws Exception;
+
+    public Object pop() throws Exception;
+
+    public Object top() throws Exception;
+
+    public boolean isEmpty();
+
+    public boolean isFull();
+
+    public void clear();
+}
